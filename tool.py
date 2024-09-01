ef8d4e728deef8acd683bca8f95138769f76840b
gjsrkjjfksn <gjisodrgnidaskndajksn@gmail.com>
2024-09-01 15:39:04 +0200

    Add files via upload

--- tool.py
+++ tool.py
@@ -1,8 +1,9 @@
 from General.args import *
 from General.read_sequences import *
-from Relaxed.run_relaxed import *
+from PD_mul_var.run_relaxed import *
 from Non_relaxed.run_non_relaxed import *
-from Extension.run_extension import *
+from PD_mul_nh.run_mul_nh import *
+from PD_single.run_pd_single import *
 
 
 def main():
@@ -15,9 +16,11 @@ def main():
 
     if args.version =="Non_relaxed":
         run_non_relaxed(mutreg_regions,full_sequences,protein_names,args)
-    elif args.version == "Extension":
+    elif args.version == "PD-mul-nh":
         run_extension(full_sequences, mutreg_regions, protein_names,args)
-    else: # relaxed version is default
+    elif args.version=="PD-single":
+        run_single(full_sequences[0], mutreg_regions[0], protein_names[0],args)  # only runs on first sequence
+    else: # PD-mul-var is default
         run_relaxed_ilp(full_sequences[0],mutreg_regions[0],protein_names[0],args) # only runs on first sequence
 
 if __name__ == '__main__':
